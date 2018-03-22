a0c8b3abd67d9b67882fd3ed86399323f0196709
NikolayPL <kolya.pluzhnik@gmail.com>
2018-03-22 19:03:29 +0200

    add view to first activity and setText QuestionView , create random method ,get value to sum or difference to value int model ,get random symbol in model.

--- app/src/main/java/com/example/nickerman/trainbrain/QuestionModel.java
+++ app/src/main/java/com/example/nickerman/trainbrain/QuestionModel.java
@@ -9,15 +9,47 @@ public class QuestionModel {
     private int mValue1;
     private int mValue2;
     private boolean mUserAnswer;
-    private String mMathimaticsSymbol;
+    private String mMathematicalSymbol;
 
 
-    private QuestionModel(int value1, int value2, String mathematicalSymbol){
+    public QuestionModel(int value1, int value2, String mathematicalSymbol){
 
         mValue1 = value1;
         mValue2 = value2;
-        mMathimaticsSymbol = mathematicalSymbol;
+        mMathematicalSymbol = mathematicalSymbol;
 
     }
 
+
+    public int getValue1() {
+        return mValue1;
+    }
+
+    public void setValue1(int value1) {
+        mValue1 = value1;
+    }
+
+    public int getValue2() {
+        return mValue2;
+    }
+
+    public void setValue2(int value2) {
+        mValue2 = value2;
+    }
+
+    public boolean isUserAnswer() {
+        return mUserAnswer;
+    }
+
+    public void setUserAnswer(boolean userAnswer) {
+        mUserAnswer = userAnswer;
+    }
+
+    public String getMathimaticsSymbol() {
+        return mMathematicalSymbol;
+    }
+
+    public void setMathimaticsSymbol(String mathimaticsSymbol) {
+        mMathematicalSymbol = mathimaticsSymbol;
+    }
 }
