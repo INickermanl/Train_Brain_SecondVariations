70a2fabd07ee7d76c430806e7fc1b9ae1598ee8e
NikolayPL <kolya.pluzhnik@gmail.com>
2018-03-27 18:16:32 +0300

    toolbar add time and quantity right answer

--- app/src/main/java/com/example/nickerman/trainbrain/_GameChooseTrueOrFalse/GameChooseTrueOrFalseActivity.java
+++ app/src/main/java/com/example/nickerman/trainbrain/_GameChooseTrueOrFalse/GameChooseTrueOrFalseActivity.java
@@ -4,8 +4,12 @@ import android.content.Intent;
 import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.ActionMenuView;
+import android.support.v7.widget.Toolbar;
+import android.support.v7.widget.*;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.TextView;
@@ -19,7 +23,9 @@ import java.util.zip.Inflater;
 
 public class GameChooseTrueOrFalseActivity extends AppCompatActivity {
 
-    private TextView mView_time;
+    private TextView mTimeView;
+    private TextView mQuantityQuestionsView;
+
 
     private TextView mViewQuestion;
     private ImageButton mTrueButton;
@@ -41,26 +47,39 @@ public class GameChooseTrueOrFalseActivity extends AppCompatActivity {
 
 
     CountDownTimer countDownTimer;
+    private Toolbar toolbar;
+    private ActionMenuView mItemQuantityQuestion;
+
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu)
-    {
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.game_true_or_false, menu);
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.game_true_or_false, menu);
         return true;
+
     }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.game_choose_true_of_false);
+        //toolbar
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
-        Intent intent = getIntent();
+
+        mTimeView = (TextView) findViewById(R.id.quantity_second_item);
+        mQuantityQuestionsView = (TextView) findViewById(R.id.quantity_questions_item);
+
+
+
+
+        //get quantity time and questions in ptiv
+         Intent intent = getIntent();
          mQuantityQuestions = Integer.parseInt(intent.getStringExtra("Settings.Game.Quantity.Question"));
          mQuantitySeconds = Integer.parseInt(intent.getStringExtra("Settings.Game.Quantity.Seconds"));
 
 
-
+            //create arrayList with random value
         for (int i = 0; i < mQuantityQuestions; i++) {
 
             mQuestionModel.add(i, new QuestionModel(getRandomNumber(),getRandomNumber(),getRandomMathematicalSymbol()));
@@ -70,11 +89,15 @@ public class GameChooseTrueOrFalseActivity extends AppCompatActivity {
         mViewQuestion = findViewById(R.id.view_question);
         mViewQuestion.setText(setViewQuestion());
 
+        //sent to toolbar quantity question
+        mQuantityQuestionsView.setText(" " + mQuantityRightAnswer);
+
+
         mTrueButton = (ImageButton) findViewById(R.id.button_true);
         mFalseButton =(ImageButton) findViewById(R.id.button_false);
-        mView_time = findViewById(R.id.view_time);
+        //mView_time = findViewById(R.id.view_time);
 
-        addTime(mView_time);
+        addTime();
 
         View.OnClickListener onClickListener = new View.OnClickListener() {
             @Override
@@ -82,11 +105,11 @@ public class GameChooseTrueOrFalseActivity extends AppCompatActivity {
                 switch(view.getId()) {
                     case R.id.button_true:      updateTrueButton(view);
                                                 countDownTimer.cancel();
-                                                addTime(mView_time);
+                                                addTime();
                                                 break;
                     case R.id.button_false :    updateFalseButton(view);
                                                 countDownTimer.cancel();
-                                                addTime(mView_time);
+                                                addTime();
                                                 break;
                 }
             }
@@ -160,7 +183,7 @@ public class GameChooseTrueOrFalseActivity extends AppCompatActivity {
 
     }
 
-    //method get value for view
+    //method get random  value for view after equally
     private int mValueForView(int ViewResult){
 
         int result = ViewResult;
@@ -192,13 +215,16 @@ public class GameChooseTrueOrFalseActivity extends AppCompatActivity {
         if(mCurrentIndex < mQuantityQuestions - 1){
             if (mQuestionModel.get(mCurrentIndex).getResult() == mQuestionModel.get(mCurrentIndex).getViewAnswer()
                     && R.id.button_true == view.getId()) {
+                //if user answer right stop time, step up index and quantity right answer
                 countDownTimer.cancel();
-                mQuantityRightAnswer++;
+                ++mQuantityRightAnswer;
                 mCurrentIndex++;
                 //update btn
                 mViewQuestion.setText(setViewQuestion());
+                mQuantityQuestionsView.setText(" " + mQuantityRightAnswer);
 
             } else {
+                //if user answer wrong just step up index
                 mCurrentIndex++;
                 countDownTimer.cancel();
                 // update btn
@@ -207,11 +233,12 @@ public class GameChooseTrueOrFalseActivity extends AppCompatActivity {
         }else if (mCurrentIndex == mQuantityQuestions - 1){
                     if (mQuestionModel.get(mCurrentIndex).getResult() == mQuestionModel.get(mCurrentIndex).getViewAnswer()
                                 && R.id.button_true == view.getId()) {
-                        mQuantityRightAnswer++;
+                        ++mQuantityRightAnswer;
                         countDownTimer.cancel();
+                        mQuantityQuestionsView.setText(" " + mQuantityRightAnswer);
 
         }
-
+            //when and our arrayList show new activity result ,did't use addtime finish method because step up index to nonexistent
             startIntentResult();
         }
 
@@ -222,34 +249,38 @@ public class GameChooseTrueOrFalseActivity extends AppCompatActivity {
         if(mCurrentIndex < mQuantityQuestions - 1) {
             if (mQuestionModel.get(mCurrentIndex).getResult() != mQuestionModel.get(mCurrentIndex).getViewAnswer()
                     && R.id.button_false == view.getId()) {
+                //if user answer right stop time, step up index and quantity right answer
                 countDownTimer.cancel();
-                mQuantityRightAnswer++;
+                ++mQuantityRightAnswer;
                 mCurrentIndex++;
                 //update btn
                 mViewQuestion.setText(setViewQuestion());
+                mQuantityQuestionsView.setText(" " + mQuantityRightAnswer);
 
             } else {
+                //if user answer wrong just step up index
                 countDownTimer.cancel();
                 mCurrentIndex++;
                 //update btn
                 mViewQuestion.setText(setViewQuestion());
             }
-        }else if (mCurrentIndex == mQuantityQuestions - 1){
+        }//when last question (if user answer right)
+        else if (mCurrentIndex == mQuantityQuestions - 1){
                     if (mQuestionModel.get(mCurrentIndex).getResult() != mQuestionModel.get(mCurrentIndex).getViewAnswer()
                             && R.id.button_false == view.getId()) {
-                    mQuantityRightAnswer++;
-                    countDownTimer.cancel();
+                        ++mQuantityRightAnswer;
+                        countDownTimer.cancel();
 
             }
 
-
+            //when and our arrayList show new activity result ,did't use addtime finish method because step up index to nonexistent
             startIntentResult();
         }
 
     }
 
 
-    public void addTime(final TextView view_time){
+    public void addTime(){
 
         int second = mQuantitySeconds;
 
@@ -257,17 +288,20 @@ public class GameChooseTrueOrFalseActivity extends AppCompatActivity {
             countDownTimer = new CountDownTimer(second * 1000, 1000) {
                 @Override
                 public void onTick(long millis) {
-                    view_time.setText(" " + (int) (millis / 1000));
+                    //do to one second
+                   // toolbar.set  //setText(" " + (int) (millis / 1000));
+                   mTimeView.setText(" " + (int) (millis / 1000));
 
                 }
 
 
                 @Override
                 public void onFinish() {
+                    //when time is over
                    if(mCurrentIndex < mQuantityQuestions - 1){
                        mCurrentIndex++;
                        mViewQuestion.setText(setViewQuestion());
-                       addTime(mView_time);
+                       addTime();
 
                    }else if(mCurrentIndex == mQuantityQuestions - 1){
                        startIntentResult();
@@ -279,8 +313,10 @@ public class GameChooseTrueOrFalseActivity extends AppCompatActivity {
     }
 
     public void startIntentResult(){
+        //so step up index arrayList for do't go to result activity twice
         mCurrentIndex += 2;
         countDownTimer.cancel();
+        //open result activity
         Intent intent  = new Intent(GameChooseTrueOrFalseActivity.this, ResultActivity.class);
         intent.putExtra(intentResult, mQuantityRightAnswer);
         startActivity(intent);
